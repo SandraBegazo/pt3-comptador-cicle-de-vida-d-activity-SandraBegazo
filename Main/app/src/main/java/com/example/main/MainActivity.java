dd2c3d4dde5790e0b624307e948e3ed00ed09e0d
dasai <dasainajo@gmail.com>
2020-10-24 21:00:27 +0200

    nothing

--- Main/app/src/main/java/com/example/main/MainActivity.java
+++ Main/app/src/main/java/com/example/main/MainActivity.java
@@ -18,7 +18,13 @@ public class MainActivity extends AppCompatActivity {
     TextView on_stop_contj;
     TextView on_destroy_contj;
 
-
+    int contadorCreate=0;
+    int contadorStart=0;
+    int contadorRestart=0;
+    int contadorResume=0;
+    int contadorPause=0;
+    int contadorStop=0;
+    int contadorDestroy=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,43 +38,66 @@ public class MainActivity extends AppCompatActivity {
         on_stop_contj=(TextView)findViewById(R.id.on_stop_cont);
         on_destroy_contj=(TextView)findViewById(R.id.on_destroy_cont);
 
-        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(),getResources().getString(R.string.ToastOnCreate), Toast.LENGTH_SHORT).show();
+
+
+        contadorCreate=contadorCreate+1;
+        on_create_contj.setText(String.valueOf(contadorCreate));
 
 
     }
     @Override
     protected void onStart(){
         super.onStart();
-        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(),getResources().getString(R.string.ToastOnStart), Toast.LENGTH_SHORT).show();
+
+
+        contadorStart=contadorStart+1;
+        on_start_contj.setText(String.valueOf(contadorStart));
     }
 
      @Override
      protected void onRestart() {
          super.onRestart();
-         Toast.makeText(this, "OnRestart", Toast.LENGTH_SHORT).show();
+         Toast.makeText(getApplicationContext(),getResources().getString(R.string.ToastOnRestart), Toast.LENGTH_SHORT).show();
+
+         contadorRestart=contadorRestart+1;
+         on_restart_contj.setText(String.valueOf(contadorRestart));
      }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(),getResources().getString(R.string.ToastOnResume), Toast.LENGTH_SHORT).show();
+
+        contadorResume=contadorResume+1;
+        on_resume_contj.setText(String.valueOf(contadorResume));
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(),getResources().getString(R.string.ToastOnPause), Toast.LENGTH_SHORT).show();
+
+        contadorPause=contadorPause+1;
+        on_pause_contj.setText(String.valueOf(contadorPause));
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(),getResources().getString(R.string.ToastOnStop), Toast.LENGTH_SHORT).show();
+
+        contadorStop=contadorStop+1;
+        on_stop_contj.setText(String.valueOf(contadorStop));
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(),getResources().getString(R.string.ToastOnDestroy), Toast.LENGTH_SHORT).show();
+
+        contadorDestroy=contadorDestroy+1;
+        on_destroy_contj.setText(String.valueOf(contadorDestroy));
     }
 }
